417fce12107eacb1256d1ee905926b130da6f27a
MoeSakuraW <1921466335@qq.com>
2021-07-01 21:01:15 +0800

    Update

--- hoshino/msghandler.py
+++ hoshino/msghandler.py
@@ -8,27 +8,28 @@ async def handle_message(bot, event: CQEvent, _):
     if event.detail_type != 'group':
         return
 
+    service_funcs = []
     for t in trigger.chain:
-        sf = t.find_handler(event)
-        if sf:
-            trigger_name = t.__class__.__name__
-            break
+        service_funcs.extend(t.find_handler(event))
 
-    if not sf:
+    if not service_funcs:
         return  # triggered nothing.
-    sf.sv.logger.info(f'Message {event.message_id} triggered {sf.__name__} by {trigger_name}.')
 
-    if sf.only_to_me and not event['to_me']:
-        return  # not to me, ignore.
+    for service_func in service_funcs:
 
-    if not sf.sv._check_all(event):
-        return  # permission denied.
+        if service_func.only_to_me and not event['to_me']:
+            continue  # not to me, ignore.
 
-    try:
-        await sf.func(bot, event)
-    except CanceledException:
-        raise
-    except Exception as e:
-        sf.sv.logger.error(f'{type(e)} occured when {sf.__name__} handling message {event.message_id}.')
-        sf.sv.logger.exception(e)
-    raise CanceledException(f'Handled by {trigger_name} of Hoshino')
+        if not service_func.sv._check_all(event):
+            continue  # permission denied.
+
+        service_func.sv.logger.info(f'Message {event.message_id} triggered {service_func.__name__}.')
+        try:
+            await service_func.func(bot, event)
+        except CanceledException:
+            raise
+        except Exception as e:
+            service_func.sv.logger.error(f'{type(e)} occured when {service_func.__name__} handling message {event.message_id}.')
+            service_func.sv.logger.exception(e)
+        raise CanceledException('Handled by Hoshino')
+        # exception raised, no need for break
--- hoshino/service.py
+++ hoshino/service.py
@@ -8,6 +8,7 @@ from functools import wraps
 import nonebot
 import pytz
 from nonebot.command import SwitchException, _FinishException, _PauseException
+from nonebot.message import CanceledException
 
 import hoshino
 from hoshino import log, priv, trigger
@@ -58,7 +59,7 @@ def _save_service_config(service):
 
 
 class ServiceFunc:
-    def __init__(self, sv: "Service", func: Callable, only_to_me: bool, normalize_text: bool=False):
+    def __init__(self, sv: "Service", func: Callable, only_to_me: bool, normalize_text: bool = False):
         self.sv = sv
         self.func = func
         self.only_to_me = only_to_me
@@ -95,14 +96,14 @@ class Service:
     储存位置：
     `~/.hoshino/service_config/{ServiceName}.json`
     """
+
     def __init__(self, name, use_priv=None, manage_priv=None, enable_on_default=None, visible=None,
                  help_=None, bundle=None):
         """
         定义一个服务
         配置的优先级别：配置文件 > 程序指定 > 缺省值
         """
-        assert not _re_illegal_char.search(
-            name), r'Service name cannot contain character in `\/:*?"<>|.`'
+        assert not _re_illegal_char.search(name), r'Service name cannot contain character in `\/:*?"<>|.`'
 
         config = _load_service_config(name)
         self.name = name
@@ -155,8 +156,7 @@ class Service:
             f'Service {self.name} is disabled at group {group_id}')
 
     def check_enabled(self, group_id):
-        return bool( (group_id in self.enable_group) or (self.enable_on_default and group_id not in self.disable_group))
-
+        return bool((group_id in self.enable_group) or (self.enable_on_default and group_id not in self.disable_group))
 
     def _check_all(self, ev: CQEvent):
         gid = ev.group_id
@@ -164,13 +164,12 @@ class Service:
 
     async def get_enable_groups(self) -> dict:
         """获取所有启用本服务的群
-        
+
         @return { group_id: [self_id1, self_id2] }
         """
         gl = defaultdict(list)
         for sid in hoshino.get_self_ids():
-            sgl = set(g['group_id']
-                      for g in await self.bot.get_group_list(self_id=sid))
+            sgl = set(g['group_id'] for g in await self.bot.get_group_list(self_id=sid))
             if self.enable_on_default:
                 sgl = sgl - self.disable_group
             else:
@@ -178,7 +177,6 @@ class Service:
             for g in sgl:
                 gl[g].append(sid)
         return gl
-
 
     def on_message(self, event='group') -> Callable:
         def deco(func) -> Callable:
@@ -188,37 +186,48 @@ class Service:
                     try:
                         return await func(self.bot, ctx)
                     except Exception as e:
-                        self.logger.error(f'{type(e)} occured when {func.__name__} handling message {ctx["message_id"]}.')
+                        self.logger.error(
+                            f'{type(e)} occured when {func.__name__} handling message {ctx["message_id"]}.')
                         self.logger.exception(e)
                     return
+
             return self.bot.on_message(event)(wrapper)
+
         return deco
 
+    def on_prefix(self, *prefix, only_to_me=False) -> Callable:
+        if len(prefix) == 1 and not isinstance(prefix[0], str):
+            prefix = prefix[0]
 
-    def on_prefix(self, prefix, only_to_me=False) -> Callable:
-        if isinstance(prefix, str):
-            prefix = (prefix, )
         def deco(func) -> Callable:
             sf = ServiceFunc(self, func, only_to_me)
             for p in prefix:
-                trigger.prefix.add(p, sf)
+                if isinstance(p, str):
+                    trigger.prefix.add(p, sf)
+                else:
+                    self.logger.error(f'Failed to add prefix trigger `{p}`, expecting `str` but `{type(p)}` given!')
             return func
+
         return deco
-    
-    
-    def on_fullmatch(self, word, only_to_me=False) -> Callable:
-        if isinstance(word, str):
-            word = (word, )
+
+    def on_fullmatch(self, *word, only_to_me=False) -> Callable:
+        if len(word) == 1 and not isinstance(word[0], str):
+            word = word[0]
+
         def deco(func) -> Callable:
             @wraps(func)
             async def wrapper(bot: HoshinoBot, event: CQEvent):
-                if len(event.message) != 1 or event.message[0].data['text']:
+                if len(event.message) != 1 or event.message[0].data.get('text'):
                     self.logger.info(f'Message {event.message_id} is ignored by fullmatch condition.')
                     return
                 return await func(bot, event)
+
             sf = ServiceFunc(self, wrapper, only_to_me)
             for w in word:
-                trigger.prefix.add(w, sf)
+                if isinstance(w, str):
+                    trigger.prefix.add(w, sf)
+                else:
+                    self.logger.error(f'Failed to add fullmatch trigger `{w}`, expecting `str` but `{type(w)}` given!')
             return func
             # func itself is still func, not wrapper. wrapper is a part of trigger.
             # so that we could use multi-trigger freely, regardless of the order of decorators.
@@ -229,40 +238,53 @@ class Service:
             # async def func(...):
             #   ...
             # ```
+
         return deco
 
+    def on_suffix(self, *suffix, only_to_me=False) -> Callable:
+        if len(suffix) == 1 and not isinstance(suffix[0], str):
+            suffix = suffix[0]
 
-    def on_suffix(self, suffix, only_to_me=False) -> Callable:
-        if isinstance(suffix, str):
-            suffix = (suffix, )
         def deco(func) -> Callable:
             sf = ServiceFunc(self, func, only_to_me)
             for s in suffix:
-                trigger.suffix.add(s, sf)
+                if isinstance(s, str):
+                    trigger.suffix.add(s, sf)
+                else:
+                    self.logger.error(f'Failed to add suffix trigger `{s}`, expecting `str` but `{type(s)}` given!')
             return func
+
         return deco
 
+    def on_keyword(self, *keywords, only_to_me=False, normalize=True) -> Callable:
+        if len(keywords) == 1 and not isinstance(keywords[0], str):
+            keywords = keywords[0]
 
-    def on_keyword(self, keywords, only_to_me=False, normalize=True) -> Callable:
-        if isinstance(keywords, str):
-            keywords = (keywords, )
         def deco(func) -> Callable:
             sf = ServiceFunc(self, func, only_to_me, normalize)
             for kw in keywords:
-                trigger.keyword.add(kw, sf)
+                if isinstance(kw, str):
+                    trigger.keyword.add(kw, sf)
+                else:
+                    self.logger.error(f'Failed to add keyword trigger `{kw}`, expecting `str` but `{type(kw)}` given!')
             return func
-        return deco
 
+        return deco
 
     def on_rex(self, rex: Union[str, re.Pattern], only_to_me=False, normalize=True) -> Callable:
         if isinstance(rex, str):
             rex = re.compile(rex)
+
         def deco(func) -> Callable:
             sf = ServiceFunc(self, func, only_to_me, normalize)
-            trigger.rex.add(rex, sf)
+            if isinstance(rex, re.Pattern):
+                trigger.rex.add(rex, sf)
+            else:
+                self.logger.error(
+                    f'Failed to add rex trigger `{rex}`, expecting `str` or `re.Pattern` but `{type(rex)}` given!')
             return func
-        return deco
 
+        return deco
 
     def on_command(self, name, *, only_to_me=False, deny_tip=None, **kwargs) -> Callable:
         kwargs['only_to_me'] = only_to_me
@@ -286,14 +308,18 @@ class Service:
                             f'Message {session.ctx["message_id"]} is handled as command by {func.__name__}.'
                         )
                         return ret
+                    except CanceledException:
+                        raise _FinishException
                     except (_PauseException, _FinishException, SwitchException) as e:
                         raise e
                     except Exception as e:
-                        self.logger.error(f'{type(e)} occured when {func.__name__} handling message {session.ctx["message_id"]}.')
+                        self.logger.error(
+                            f'{type(e)} occured when {func.__name__} handling message {session.ctx["message_id"]}.')
                         self.logger.exception(e)
-            return nonebot.on_command(name, **kwargs)(wrapper)
-        return deco
 
+            return nonebot.on_command(name, **kwargs)(wrapper)
+
+        return deco
 
     def on_natural_language(self, keywords=None, **kwargs) -> Callable:
         def deco(func) -> Callable:
@@ -306,17 +332,24 @@ class Service:
                             f'Message {session.ctx["message_id"]} is handled as natural language by {func.__name__}.'
                         )
                         return ret
+                    except CanceledException:
+                        raise _FinishException
+                    except (_PauseException, _FinishException, SwitchException) as e:
+                        raise e
                     except Exception as e:
-                        self.logger.error(f'{type(e)} occured when {func.__name__} handling message {session.ctx["message_id"]}.')
+                        self.logger.error(
+                            f'{type(e)} occured when {func.__name__} handling message {session.ctx["message_id"]}.')
                         self.logger.exception(e)
-            return nonebot.on_natural_language(keywords, **kwargs)(wrapper)
-        return deco
 
+            return nonebot.on_natural_language(keywords, **kwargs)(wrapper)
+
+        return deco
 
     def scheduled_job(self, *args, **kwargs) -> Callable:
         kwargs.setdefault('timezone', pytz.timezone('Asia/Shanghai'))
         kwargs.setdefault('misfire_grace_time', 60)
         kwargs.setdefault('coalesce', True)
+
         def deco(func: Callable[[], Any]) -> Callable:
             @wraps(func)
             async def wrapper():
@@ -328,16 +361,17 @@ class Service:
                 except Exception as e:
                     self.logger.error(f'{type(e)} occured when doing scheduled job {func.__name__}.')
                     self.logger.exception(e)
-            return nonebot.scheduler.scheduled_job(*args, **kwargs)(wrapper)
-        return deco
 
+            return nonebot.scheduler.scheduled_job(*args, **kwargs)(wrapper)
+
+        return deco
 
     async def broadcast(self, msgs, TAG='', interval_time=0.5, randomiser=None):
         bot = self.bot
         if isinstance(msgs, (str, MessageSegment, Message)):
-            msgs = (msgs, )
-        glist = await self.get_enable_groups()
-        for gid, selfids in glist.items():
+            msgs = (msgs,)
+        groups = await self.get_enable_groups()
+        for gid, selfids in groups.items():
             try:
                 for msg in msgs:
                     await asyncio.sleep(interval_time)
@@ -350,7 +384,6 @@ class Service:
                 self.logger.error(f"群{gid} 投递{TAG}失败：{type(e)}")
                 self.logger.exception(e)
 
-
     def on_request(self, *events):
         def deco(func):
             @wraps(func)
@@ -358,10 +391,11 @@ class Service:
                 if not self.check_enabled(session.event.group_id):
                     return
                 return await func(session)
+
             return nonebot.on_request(*events)(wrapper)
+
         return deco
-    
-    
+
     def on_notice(self, *events):
         def deco(func):
             @wraps(func)
@@ -369,16 +403,19 @@ class Service:
                 if not self.check_enabled(session.event.group_id):
                     return
                 return await func(session)
-            return nonebot.on_notice(*events)(wrapper)
-        return deco
 
+            return nonebot.on_notice(*events)(wrapper)
+
+        return deco
 
 
 sulogger = log.new_logger('sucmd', hoshino.config.DEBUG)
 
+
 def sucmd(name, force_private=True, **kwargs) -> Callable:
     kwargs['privileged'] = True
     kwargs['only_to_me'] = False
+
     def deco(func) -> Callable:
         @wraps(func)
         async def wrapper(session: CommandSession):
@@ -389,10 +426,14 @@ def sucmd(name, force_private=True, **kwargs) -> Callable:
                 return
             try:
                 return await func(session)
+            except CanceledException:
+                raise _FinishException
             except (_PauseException, _FinishException, SwitchException):
                 raise
             except Exception as e:
                 sulogger.error(f'{type(e)} occured when {func.__name__} handling message {session.event.message_id}.')
                 sulogger.exception(e)
+
         return nonebot.on_command(name, **kwargs)(wrapper)
+
     return deco
